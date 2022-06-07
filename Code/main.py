8b3003c468b85713bac282b5327654fa455e58bb
ce424-002-97110625 <m.mohammadi1378@outlook.com>
2022-06-07 14:23:07 +0430

    undo comment

--- Code/main.py
+++ Code/main.py
@@ -5,7 +5,7 @@ from video_controller import main as video_main
 from network import main as network_main
 
 # Start Speech Recognition
-# threading.Thread(target=speech_main).start()
+threading.Thread(target=speech_main).start()
 
 # Start Video Capturing
 threading.Thread(target=video_main).start()
